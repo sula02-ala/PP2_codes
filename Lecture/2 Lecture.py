9655fb75c2cea4ce946109b9c88d55626a38ba97
Sultan Maksumov <s_maksumov@kbtu.kz>
2026-02-01 21:02:00 +0500

    Create 2 Lecture.py

---
+++ Lecture/2 Lecture.py
@@ -0,0 +1,33 @@
+# # # # # class cinema:
+# # # # #     name = "Fight club"
+# # # # #     duration = 1.0
+# # # # #     director = "Default director"
+# # # # #     genre= "Default genre"
+
+# # # # #     def set_properties(self,movie_name,movie_duration,movie_director,movie_genre):
+# # # # #         self.name = movie_name
+# # # # #         self.duration = movie_duration
+# # # # #         self.director= movie_director
+# # # # #         self.genre = movie_genre
+
+# # # # #     def get_properties(self):
+# # # # #         print(self.name,self.duration,self.director,self.genre)
+
+
+
+# cup1="Cola"
+# cup2="Sprite"
+
+# cup1,cup2 = cup2,cup1 - замена 
+# print(cup1,cup2) 
+
+# print(-3**2) - степень отрицательного числа
+
+# print(int("0b11",base=2))
+# print(int("0o12",base=8))
+
+# a=int(input())
+# b=int(input())
+
+# print("PP2" if a>b else "PP1") - условие внутри принта
+
