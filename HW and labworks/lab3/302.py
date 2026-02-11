36ad7ad019020b19dea9e18788546b216a97b3c3
Sultan Maksumov <s_maksumov@kbtu.kz>
2026-02-11 12:13:20 +0500

    Create 302.py

---
+++ HW and labworks/lab3/302.py
@@ -0,0 +1,13 @@
+def isUsual(num):
+    for p in [2, 3, 5]:
+        while num % p == 0:
+            num //= p
+    return num == 1
+
+
+n = int(input())
+
+if isUsual(n):
+    print("Yes")
+else:
+    print("No")
