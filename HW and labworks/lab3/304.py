a31f06e2c48c402eb711e6ad044ecd44000a5126
Sultan Maksumov <s_maksumov@kbtu.kz>
2026-02-14 12:33:39 +0500

    Create 304.py

---
+++ HW and labworks/lab3/304.py
@@ -0,0 +1,9 @@
+class StringHandler:
+    def getString(n):
+        n.text=input()
+    def printString(n):
+        print(n.text.upper())
+
+a=StringHandler()
+a.getString()
+a.printString()            
