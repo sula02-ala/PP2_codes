66c9f6733ca67eac70309f5ffbabcb89073a7913
Sultan Maksumov <s_maksumov@kbtu.kz>
2026-02-28 20:19:25 +0500

    Create 4 Lecture.py

---
+++ Lecture/4 Lecture.py
@@ -0,0 +1,32 @@
+# import statistics
+# print(statistics.mean([85,90]))
+
+# import random
+# coin=random.choice([1,2,3,4,5,6])
+# print(coin)
+
+# import random
+# cards=["clubs","diamonts","Heart","spades"]
+# random.shuffle(cards)
+# for card in cards:
+#     print(card)
+
+# import sys
+# try:
+#     print("Hello,my name is",sys.argv[3])
+# except IndexError:
+#     print("Woops,index is wrong!")
+
+# import sys
+# try:
+#     sys.exit("Hello,my name is",sys.argv[3])
+# except IndexError:
+#     sys.exit("Woops,index is wrong!")
+
+# import re
+# p=re.compile('kaz{5}')
+
+# word='kazzzzz'
+
+# print(p.match(word))
+
